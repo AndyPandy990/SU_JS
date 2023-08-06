b398cfcd81086a080cc27c12c3d52ed3b54d9f7b
Gabovv <122400812+AndyPandy990@users.noreply.github.com>
2023-08-06 09:31:52 +0300

    Update 07-hotelRoom.js

--- JS-Basics/EX03-ConditionalStatementsAdvanced/07-hotelRoom.js
+++ JS-Basics/EX03-ConditionalStatementsAdvanced/07-hotelRoom.js
@@ -10,53 +10,52 @@ function hotelRoom(input) {
     let allStayApartment = 0;
 
     if (month == "May" || month == "October") {
-
         studio = 50;
         apartment = 65;
+        
         if (nightStaysCount >= 8 && nightStaysCount < 14) {
-
             studioDiscount = studio * (95 / 100);
             allStayStudio = studioDiscount * nightStaysCount;
             allStayApartment = apartment * nightStaysCount;
+            
         } else if (nightStaysCount >= 15) {
-
             studioDiscount = studio * (70 / 100);
             apartmentDiscount = apartment * (90 / 100);
 
             allStayStudio = studioDiscount * nightStaysCount;
             allStayApartment = apartmentDiscount * nightStaysCount;
+            
         } else {
             allStayStudio = studio * nightStaysCount;
             allStayApartment = apartment * nightStaysCount;
         }
         
     } else if (month == "June" || month == "September") {
-
         studio = 75.20;
         apartment = 68.70;
+        
         if (nightStaysCount >= 15) {
-
             studioDiscount = studio * (80 / 100);
             apartmentDiscount = apartment * (90 / 100);
 
             allStayStudio = studioDiscount * nightStaysCount;
             allStayApartment = apartmentDiscount * nightStaysCount;
+            
         } else {
             allStayStudio = studio * nightStaysCount;
             allStayApartment = apartment * nightStaysCount;
         }
 
     } else if (month == "July" || month == "August") {
-
         studio = 76;
         apartment = 77;
+        
         if (nightStaysCount >= 15) {
-
             apartmentDiscount = apartment * (90 / 100);
             allStayApartment = apartmentDiscount * nightStaysCount;
+            
         } else {
-
-        allStayApartment = apartment * nightStaysCount;
+            allStayApartment = apartment * nightStaysCount;
         }
         
         allStayStudio = studio * nightStaysCount;
