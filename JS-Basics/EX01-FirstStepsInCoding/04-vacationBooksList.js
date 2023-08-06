6944a76ab549f13d877aab03084907bfefa0eb4b
Gabovv <122400812+AndyPandy990@users.noreply.github.com>
2023-08-06 09:25:56 +0300

    Update 04-vacationBooksList.js

--- JS-Basics/EX01-FirstStepsInCoding/04-vacationBooksList.js
+++ JS-Basics/EX01-FirstStepsInCoding/04-vacationBooksList.js
@@ -1,15 +1,8 @@
 function vacayBookList(input) {
-        // br stranici - 1_1000
-        //stranici za 1 chas - 1_1000
-        //broi na dnite - 1_1000
+
         let pagesCount = Number(input[0]);
         let pagesPerHour = Number(input[1]);
         let days = Number(input[2]);
-
-        //pagesCount = 60
-        //pagesPerHour = 20
-        //hours = 3
-
         let totalHours = pagesCount / pagesPerHour;
         let hoursPerDay = totalHours / days;
         
